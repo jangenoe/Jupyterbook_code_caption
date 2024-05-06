88590bbd5f45b36e7c4c3dc0a60ac0ce5c547938
Jan Genoe <jan.genoe@gmail.com>
2024-05-06 14:07:52 +0200

    first draft

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,17 @@
-import type { FieldProps, WidgetProps } from '@rjsf/utils';
 import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-import {
-  IFormRendererRegistry,
-  IFormRenderer
-} from '@jupyterlab/ui-components';
 
 /**
- * Initialization of a simple metadata form plugin.
+ * Initialization of a caption metadata form plugin.
  */
-const simple: JupyterFrontEndPlugin<void> = {
-  id: 'Jupyterbook_code_caption:simple',
+const caption: JupyterFrontEndPlugin<void> = {
+  id: 'Jupyterbook_code_caption:caption',
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
     console.log('Jupyterbook_code_caption metadata-form example activated');
   }
 };
 
-export default [simple];
+export default [caption];
